7f1188687eb6a0cd0a3bb6644e7c787fda076527
Alicia <alicia.arenzana@gmail.com>
2020-10-23 10:08:43 +0200

    fixed path to folder

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM tensorflow/tensorflow:latest-gpu-py3-jupyter
 
 #Source image: https://github.com/tensorflow/tensorflow/blob/master/tensorflow/tools/dockerfiles/dockerfiles/gpu-jupyter.Dockerfile
-MAINTAINER Vizzuality Science Team info@vizzuality.com
 
 RUN apt-get update && \
     apt-get install -y --no-install-recommends \
@@ -25,8 +24,9 @@ RUN pip install jupyter_contrib_nbextensions version_information jupyterlab  && 
     jupyter contrib nbextension install --sys-prefix
 RUN mkdir -p /tf/deepsky
 COPY . /tf/deepsky
-RUN pip install -e /tf/deepsky
-RUN pip install -r /tf/deepsky/requirements.txt
 COPY jupyter_notebook_config.py /etc/jupyter/
+RUN pip install -e /tf/deepsky
+# RUN pip install -r /tf/deepsky/requirements.txt
+
 EXPOSE 8888
 
--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #docker build --rm -f Dockerfile -t deepsky . && docker run -v ~/Projects/DeepSky:/tf/deepsky  --rm -p 6868:8888 --gpus all -it --name mlnotebooks vizzuality/mlnotebooks
-
+MYPATH=$(pwd)
 set -e
 
 case "$1" in
     gpu)
         echo "Running container with GPUs"
-        docker build --rm -f Dockerfile -t vizzuality/mlnotebooks . && docker run -u $(id -u):$(id -g) --env-file=.env -v ~/Proiektuak/Skydipper/DeepSky:/tf/deepsky  --rm -p $2:8888 --gpus all -it --name mlnotebooks vizzuality/mlnotebooks
+        docker build --rm -f Dockerfile -t vizzuality/mlnotebooks . && docker run -u $(id -u):$(id -g) --env-file=.env -v $MYPATH:/tf/deepsky  --rm -p $2:8888 --gpus all -it --name mlnotebooks vizzuality/mlnotebooks
         ;;
     cpu)
         echo "Running container with CPU"
-        docker build -t vizzuality/mlnotebooks . && docker run -v ~/Proiektuak/Skydipper/DeepSky:/tf/deepsky  --rm -p 6868:8888 -it --name mlnotebooks vizzuality/mlnotebooks
+        docker build -t vizzuality/mlnotebooks . && docker run -v $MYPATH:/tf/deepsky  --rm -p 6868:8888 -it --name mlnotebooks vizzuality/mlnotebooks
         ;;
     *)
         echo "use gpu or cpu flag; exit with 0..."
